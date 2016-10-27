514f1fdc5ff619cab06e81c479b0e82cb3e22c4d
Rob Winch <rwinch@gopivotal.com>
2016-10-27 09:21:55 -0500

    Fix NPE

--- src/main/java/sample/VaultConfig.java
+++ src/main/java/sample/VaultConfig.java
@@ -1,9 +1,10 @@
 package sample;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
@@ -11,9 +12,8 @@ import org.springframework.vault.config.AbstractVaultConfiguration;
 
 @Configuration
 @ComponentScan
-public class VaultConfig extends AbstractVaultConfiguration {
-	@Autowired
-	Environment env;
+public class VaultConfig extends AbstractVaultConfiguration implements ApplicationContextAware {
+	ApplicationContext context;
 
 	/**
 	 * Specify an endpoint for connecting to Vault.
@@ -31,6 +31,10 @@ public class VaultConfig extends AbstractVaultConfiguration {
 	 */
 	@Override
 	public ClientAuthentication clientAuthentication() {
-		return new TokenAuthentication(env.getProperty("vault.token"));
+		return new TokenAuthentication(context.getEnvironment().getProperty("vault.token"));
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
 	}
 }
